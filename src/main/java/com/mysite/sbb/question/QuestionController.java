df49e23ec19c1aba1af097bb490c6dbf28979f21
leedahye426 <9535957@naver.com>
2023-11-15 15:14:14 +0900

    answer list

---
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -0,0 +1,32 @@
+package com.mysite.sbb.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequestMapping("/question")
+@Controller
+@RequiredArgsConstructor
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Question> questions = questionService.getList();
+        model.addAttribute("questions", questions);
+        return "question_list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
+    }
+}
