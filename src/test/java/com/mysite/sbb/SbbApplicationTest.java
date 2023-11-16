495f291782635a82824700fff1fcb4c6f3b9f7a0
leedahye426 <9535957@naver.com>
2023-11-16 13:38:10 +0900

    paging

--- src/test/java/com/mysite/sbb/SbbApplicationTest.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTest.java
@@ -1,42 +1,21 @@
 package com.mysite.sbb;
 
-import com.mysite.sbb.question.Question;
-import com.mysite.sbb.question.QuestionRepository;
-import jakarta.transaction.Transactional;
+import com.mysite.sbb.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 @SpringBootTest
 public class SbbApplicationTest {
-
     @Autowired
-    private QuestionRepository questionRepository;
+    private QuestionService questionService;
 
-    @Transactional
     @Test
-    void testJPa() {
-        Question q1 = new Question();
-        q1.setSubject("질문1 제목");
-        q1.setContent("질문1 내용");
-        q1.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q1);
-
-        Question q2 = new Question();
-        q2.setSubject("질문2 제목");
-        q2.setContent("질문2 내용");
-        q2.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q2);
-
-        List<Question> questions = this.questionRepository.findAll();
-        assertEquals(2, questions.size());
-
-        Question question = questions.get(0);
-        assertEquals("질문1 제목", question.getSubject());
+    void testJpa() {
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.create(subject, content);
+        }
     }
 }
