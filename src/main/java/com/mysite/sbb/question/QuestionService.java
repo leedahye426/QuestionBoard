1edfaac3e88dce31258e93f220a958de6cb338f8
leedahye426 <9535957@naver.com>
2023-11-19 01:49:48 +0900

    question modify

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -46,5 +46,12 @@ public class QuestionService {
         questionRepository.save(question);
     }
 
+    public void modify(Question question, String subject, String content) {
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setModifyDate(LocalDateTime.now());
+
+        questionRepository.save(question);
+    }
 
 }
